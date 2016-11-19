5c950cda1673e4f6241aa9c7fb5c001fa1a44f8e
ankitjain28may <ankitjain28may77@gmail.com>
2016-11-20 04:21:17 +0530

    Can be used with firefox, phantomJs, chrome

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import time
-
 from selenium import webdriver
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 import selenium.webdriver.chrome.service as service
 from bs4 import BeautifulSoup
 import requests
@@ -17,14 +17,16 @@ def get_soup(url,header):
 header={'User-Agent':"Mozilla/6.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36"
 }
 
-ActualProducts=[]# contains the link for the products
+ActualProducts=[] # contains the link for the products
 
 
-query = input("Enter Product Name\n")# you can change the query for the product  here
+query = input("Enter Product Name\n") # you can change the query for the product  here
 query= query.split()
 query='+'.join(query)
 url = "https://www.snapdeal.com/search?keyword="+query+"&santizedKeyword=&catId=&categoryId=0&suggested=false&vertical=&noOfResults=20&searchState=&clickSrc=go_header&lastKeyword=&prodCatId=&changeBackToAll=false&foundInAll=false&categoryIdSearched=&cityPageUrl=&categoryUrl=&url=&utmContent=&dealDetail=&sort=rlvncy"
-print(url)
+
+# print(url)
+
 soup = get_soup(url,header)
 print(soup.title.string)
 product = soup.find_all("a", class_="dp-widget-link")
@@ -39,6 +41,7 @@ for a in product:
         pass
 
 # print(ActualProducts)
+
 for i ,(link,name) in enumerate(ActualProducts):
     try:
         print("%d - %s" % (i, name))
@@ -52,22 +55,30 @@ review = int(input("Enter the product number to get reviews\n"))
 link,name = ActualProducts[review]
 print(link)
 # Start the WebDriver and load the page
-# wd = webdriver.Firefox()
-# wd.get("http://www.google.com")
-service = service.Service('chromedriver.exe')
-service.start()
-capabilities = {'chrome.binary': '/chromedriver.exe'}
-driver = webdriver.Remote(service.service_url, capabilities)
 
-driver.get(link);
-time.sleep(5) # Let the user actually see something!
-# driver.quit()
+# Chrome Browser
 
-# Wait for the dynamically loaded elements to show up
-# WebDriverWait wait = new WebDriverWait(driver,10);
-# WebDriverWait(driver, 10).until(
-#     EC.visibility_of_element_located((By.CLASS_NAME, "user-review")))
-# print(driver.page_source)
+# service = service.Service('chromedriver.exe')
+# service.start()
+# capabilities = {'chrome.binary': '/chromedriver.exe'}
+# driver = webdriver.Remote(service.service_url, capabilities)
+
+# PhantomJS
+
+driver = webdriver.PhantomJS(executable_path=r'C:\Users\ankit\Downloads\phantomjs-2.1.1-windows (1)\phantomjs-2.1.1-windows\bin\phantomjs.exe')
+driver.set_window_size(1120, 550)
+
+
+# Firefox Browser
+
+# binary = FirefoxBinary('C:\Program Files (x86)\Mozilla Firefox\Firefox.exe')
+# driver = webdriver.Firefox(firefox_binary=binary)
+
+
+# Let the user actually see something!
+driver.get(link)
+time.sleep(5)
+
 # And grab the page HTML source
 html_page = driver.page_source
 driver.quit()
